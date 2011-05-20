4a588eef4996682b021bc28dd60d3712a2f0f431
Geoff Hill <geoff@geoffhill.org>
2011-05-20 14:37:59 -0500

    started work on L5

---
+++ L5-test.rkt
@@ -0,0 +1,11 @@
+#lang plai
+(print-only-errors #t)
+
+;;; EECS 322 L5->L4 Compiler -- Test
+;;; Geoff Hill <GeoffreyHill2012@u.northwestern.edu>
+;;; Spring 2011
+
+(require (file "types.rkt"))
+(require (file "L5.rkt"))
+
+(printf "tests completed~n")
---
+++ L5.rkt
@@ -0,0 +1,44 @@
+#lang plai
+
+;;; EECS 322 L5->L4 Compiler
+;;; Geoff Hill <GeoffreyHill2012@u.northwestern.edu>
+;;; Spring 2011
+
+(require (file "types.rkt"))
+(require (file "preds.rkt"))
+(require (file "utils.rkt"))
+
+;;;
+;;; L5 -> L4 COMPILATION
+;;;
+
+#|
+(define/contract (compile-L5prog prog)
+  (L5prog? . -> . L4prog?)
+  ...)
+
+(define/contract (compile-L5fn fn)
+  (L5fn? . -> . L4fn?)
+  ...)
+
+(define/contract (compile-L5expr expr)
+  (L5expr? . -> . L4expr?)
+  ...)
+|#
+  
+;;;
+;;; EXTERNAL INTERFACE
+;;;
+
+#|
+(define/contract (main fname)
+  (string? . -> . void?)
+  (call-with-input-file fname main/compile-L5))
+
+(define/contract (main/compile-L5 port)
+  (input-port? . -> . void?)
+  (pretty-write
+   (format-L4prog
+    (compile-L5prog
+     (build-L5prog (read port))))))
+|#
