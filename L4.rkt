9e8ec62f07a90718c2ece25fbfb234498b5c67fc
Geoff Hill <geoff@geoffhill.org>
2011-05-21 13:02:41 -0500

    started context lifting work in L4

--- L4.rkt
+++ L4.rkt
@@ -8,29 +8,45 @@
 (require (file "preds.rkt"))
 (require (file "utils.rkt"))
 
+(define-type L4ctxt
+  [mt-ctxt]
+  [let-ctxt (id L4-x?)
+            (binding L4expr?)
+            (body L4expr?)
+            (ctxt L4ctxt?)]
+  [if-ctxt (test L4expr?)
+           (then L4expr?)
+           (else L4expr?)
+           (ctxt L4ctxt?)]
+  [fn-ctxt (op L4expr?)
+           (args (listof L4expr?))
+           (ctxt L4ctxt?)])
+
 ;;;
 ;;; L4 -> L3 COMPILATION
 ;;;
 
-#|
 (define-with-contract (compile-L4prog prog)
   (L4prog? . -> . L3prog?)
-  ...)
+  (type-case L4prog prog
+    [l4prog (main others)
+            (l3prog (compile-L4fn main)
+                    (map compile-L4fn others))]))
 
 (define-with-contract (compile-L4fn fn)
   (L4fn? . -> . L3fn?)
-  ...)
+  (type-case L4fn fn
+    [l4mainfn (body) (l3mainfn (compile-L4expr body))]
+    [l4fn (lbl args body) (l3fn lbl args (compile-L4expr body))]))
 
 (define-with-contract (compile-L4expr expr)
   (L4expr? . -> . L3expr?)
-  ...)
-|#
+  expr)
   
 ;;;
 ;;; EXTERNAL INTERFACE
 ;;;
 
-#|
 (define-with-contract (main fname)
   (string? . -> . void?)
   (call-with-input-file fname main/compile-L4))
@@ -41,4 +57,3 @@
    (format-L3prog
     (compile-L4prog
      (build-L4prog (read port))))))
-|#
