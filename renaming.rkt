9d8de5ae32972f42c1227f9c2cf716d1ece88aa1
Geoff Hill <geoff@geoffhill.org>
2011-06-02 15:46:06 -0500

    added renaming support for L1, fixed L2 renaming

--- renaming.rkt
+++ renaming.rkt
@@ -8,10 +8,10 @@
 (require (file "utils.rkt"))
 (require (file "types.rkt"))
 
+;;; renaming always does function labels first
+
 ;;; where possible, the renaming mechanism clears up any
 ;;; instances of variable shadowing due to scope
-
-;; TODO: process function labels first in L2 like L3-L4
 
 ;; dictionary abstract data type
 (define dict make-hash)
@@ -36,6 +36,73 @@
     v))
 
 ;;;
+;;; L1 RENAMING
+;;;
+
+(define L1-lbl-prefix ':L)
+
+(define-with-contract (rename-L1prog prog)
+  (L1prog? . -> . L1prog?)
+  (type-case L1prog prog
+    [l1prog (main others)
+            (let ([lblfn (make-counter L1-lbl-prefix)]
+                  [lblmap (dict)]
+                  [main-stmts (l1mainfn-stmts main)])
+              (when (and (not (null? main-stmts))
+                         (l1s-label? (first main-stmts)))
+                (dict-extend! lblmap (l1s-label-lbl (first main-stmts)) lblfn))
+              (for/list ([fn others])
+                (dict-extend! lblmap (l1fn-lbl fn) lblfn))
+              (l1prog (rename-L1fn main lblfn lblmap)
+                      (for/list ([fn others])
+                        (rename-L1fn fn lblfn lblmap))))]))
+
+(define-with-contract (rename-L1fn fn lblfn lblmap)
+  (L1fn? (-> symbol?) dict? . -> . L1fn?)
+  (define (process-stmt stmt)
+    (rename-L1stmt stmt lblfn lblmap))
+  (type-case L1fn fn
+    [l1mainfn (stmts) (l1mainfn (map process-stmt stmts))]
+    [l1fn (lbl stmts)
+          (l1fn (dict-extend! lblmap lbl lblfn)
+                (map process-stmt stmts))]))
+
+(define-with-contract (rename-L1stmt stmt lblfn lblmap)
+  (L1stmt? (-> symbol?) dict? . -> . L1stmt?)
+  (define (replace x)
+    (if (label? x)
+        (dict-extend! lblmap x lblfn)
+        x))
+  (type-case L1stmt stmt
+    [l1s-assign (lhs rhs)
+                (l1s-assign (replace lhs) (replace rhs))]
+    [l1s-memget (lhs base offset)
+                (l1s-memget (replace lhs) (replace base) offset)]
+    [l1s-memset (base offset rhs)
+                (l1s-memset (replace base) offset (replace rhs))]
+    [l1s-aop (lhs op rhs)
+             (l1s-aop (replace lhs) op (replace rhs))]
+    [l1s-sop (lhs op rhs)
+             (l1s-sop (replace lhs) op (replace rhs))]
+    [l1s-cmp (lhs c1 op c2)
+             (l1s-cmp (replace lhs) (replace c1) op (replace c2))]
+    [l1s-label (lbl)
+               (l1s-label (replace lbl))]
+    [l1s-goto (lbl)
+              (l1s-goto (replace lbl))]
+    [l1s-cjump (c1 op c2 lbl1 lbl2)
+               (l1s-cjump (replace c1) op (replace c2) (replace lbl1) (replace lbl2))]
+    [l1s-call (dst) (l1s-call (replace dst))]
+    [l1s-tcall (dst) (l1s-tcall (replace dst))]
+    [l1s-return () (l1s-return)]
+    [l1s-print (lhs arg1)
+               (l1s-print (replace lhs) (replace arg1))]
+    [l1s-alloc (lhs arg1 arg2)
+               (l1s-alloc (replace lhs) (replace arg1) (replace arg2))]
+    [l1s-arrayerr (lhs arg1 arg2)
+                  (l1s-arrayerr (replace lhs) (replace arg1) (replace arg2))]))
+
+;;;
 ;;; L2 RENAMING
 ;;;
 
@@ -47,19 +114,28 @@
   (type-case L2prog prog
     [l2prog (main others)
             (let ([lblfn (make-counter L2-lbl-prefix)]
-                  [lblmap (dict)])
+                  [lblmap (dict)]
+                  [main-stmts (l2mainfn-stmts main)])
+              (when (and (not (null? main-stmts))
+                         (l2s-label? (first main-stmts)))
+                (dict-extend! lblmap (l2s-label-lbl (first main-stmts)) lblfn))
+              (for/list ([fn others])
+                (dict-extend! lblmap (l2fn-lbl fn) lblfn))
               (l2prog (rename-L2fn main lblfn lblmap)
                       (for/list ([fn others])
                         (rename-L2fn fn lblfn lblmap))))]))
 
 (define-with-contract (rename-L2fn fn lblfn lblmap)
   (L2fn? (-> symbol?) dict? . -> . L2fn?)
-  (type-case L2fn fn
-    [l2fn (stmts)
-          (l2fn (let ([varfn (make-counter L2-var-prefix)]
-                      [varmap (dict)])
-                  (for/list ([stmt stmts])
-                    (rename-L2stmt stmt lblfn varfn lblmap varmap))))]))
+  (let ([varfn (make-counter L2-var-prefix)]
+        [varmap (dict)])
+    (define (process-stmt stmt)
+      (rename-L2stmt stmt lblfn varfn lblmap varmap))
+    (type-case L2fn fn
+      [l2mainfn (stmts) (l2mainfn (map process-stmt stmts))]
+      [l2fn (lbl stmts)
+            (l2fn (dict-extend! lblmap lbl lblfn)
+                  (map process-stmt stmts))])))
 
 (define-with-contract (rename-L2stmt stmt lblfn varfn lblmap varmap)
   (L2stmt? (-> symbol?) (-> symbol?) dict? dict? . -> . L2stmt?)
