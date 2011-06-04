9866d76572c2bc2ad8559c4579dc64b7a609870f
Geoff Hill <geoff@geoffhill.org>
2011-06-04 11:47:52 -0500

    static linking on Linux

--- driver.sh
+++ driver.sh
@@ -5,11 +5,11 @@ if [ `uname` == "Darwin" ]
   then
     RACKET_PATH="/Applications/Racket v5.1/bin/racket"
     AS="as -arch i386"
-    CC="gcc -arch i386 -O0"
+    CC="gcc -arch i386 -m32 -O0"
   else
     RACKET_PATH="/opt/racket-5.1/bin/racket"
     AS="as"
-    CC="gcc4 -O0"
+    CC="gcc4 -static -O0"
 fi
 
 FPREFIX=`echo $RANDOM`
@@ -24,7 +24,7 @@ if [ -z "$1" ];
         "$RACKET_PATH" -tm "$DIR/driver.rkt" "$COMMAND" "$1" > "$FPREFIX.asm"
         # TODO: fix GCC optimization
         $AS -o "$FPREFIX.o" "$FPREFIX.asm"
-        $CC -o "$EXEC" "$FPREFIX.o" "L1-runtime.c"
+        $CC -o "$EXEC" "L1-runtime.c" "$FPREFIX.o"
         chmod +x "$EXEC"
         rm -f "$FPREFIX.asm" "$FPREFIX.o"
       else
