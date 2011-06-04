534b2f00c043dcab792ea7652a8abf91968a9911
Geoff Hill <geoff@geoffhill.org>
2011-06-04 13:37:01 -0500

    fixed L1 label into mem problem

--- L1.rkt
+++ L1.rkt
@@ -66,102 +66,104 @@
   (L1stmt? (-> symbol?) . -> . string?)
   (type-case L1stmt stmt
     [l1s-assign (lhs rhs)
-                 (if (and (num? rhs) (zero? rhs))
-                     (format "  xorl ~a, ~a~n" (asm-s lhs) (asm-s lhs))
-                     (format "  movl ~a, ~a~n"
-                             (if (label? rhs)
-                                 (string-append "$" (asm-s rhs))
-                                 (asm-s rhs))
-                             (asm-s lhs)))]
+                (if (and (num? rhs) (zero? rhs))
+                    (format "  xorl ~a, ~a~n" (asm-s lhs) (asm-s lhs))
+                    (format "  movl ~a, ~a~n"
+                            (if (label? rhs)
+                                (string-append "$" (asm-s rhs))
+                                (asm-s rhs))
+                            (asm-s lhs)))]
     [l1s-memget (lhs base offset)
-                 (format "  movl ~a(~a), ~a~n" offset (asm-s base) (asm-s lhs))]
+                (format "  movl ~a(~a), ~a~n" offset (asm-s base) (asm-s lhs))]
     [l1s-memset (base offset rhs)
-                 (format "  movl ~a, ~a(~a)~n" (asm-s rhs) offset (asm-s base))]
+                (if (label? rhs)
+                    (format "  movl $~a, ~a(~a)~n" (asm-s rhs) offset (asm-s base))
+                    (format "  movl ~a, ~a(~a)~n" (asm-s rhs) offset (asm-s base)))]
     [l1s-aop (lhs op rhs)
-              (format (case op
-                        [(+=) "  addl ~a, ~a~n"]
-                        [(-=) "  subl ~a, ~a~n"]
-                        [(*=) "  imul ~a, ~a~n"]
-                        [(&=) "  andl ~a, ~a~n"])
-                      (asm-s rhs)
-                      (asm-s lhs))]
+             (format (case op
+                       [(+=) "  addl ~a, ~a~n"]
+                       [(-=) "  subl ~a, ~a~n"]
+                       [(*=) "  imul ~a, ~a~n"]
+                       [(&=) "  andl ~a, ~a~n"])
+                     (asm-s rhs)
+                     (asm-s lhs))]
     [l1s-sop (lhs op rhs)
-              (format (case op
-                        [(<<=) "  sall ~a, ~a~n"]
-                        [(>>=) "  sarl ~a, ~a~n"])
-                      (asm-s-lsb rhs)
-                      (asm-s lhs))]
+             (format (case op
+                       [(<<=) "  sall ~a, ~a~n"]
+                       [(>>=) "  sarl ~a, ~a~n"])
+                     (asm-s-lsb rhs)
+                     (asm-s lhs))]
     [l1s-cmp (lhs c1 op c2)
-              (cond [(and (num? c1) (num? c2))
-                     (format "  movl ~a, ~a~n"
-                             (if ((case op [(<) <] [(<=) <=] [(=) =]) c1 c2)
-                                 (asm-s 1)
-                                 (asm-s 0))
-                             (asm-s lhs))]
-                    [(num? c1)
-                     (format "  cmpl ~a, ~a~n  ~a ~a~n  andl ~a, ~a~n"
-                             (asm-s c1) (asm-s c2)
-                             (case op [(<) "setg"] [(<=) "setge"] [(=) "sete"]) (asm-s-lsb lhs)
-                             (asm-s 1) (asm-s lhs))]
-                    [else
-                     (format "  cmpl ~a, ~a~n  ~a ~a~n  andl ~a, ~a~n"
-                             (asm-s c2) (asm-s c1)
-                             (case op [(<) "setl"] [(<=) "setle"] [(=) "sete"]) (asm-s-lsb lhs)
-                             (asm-s 1) (asm-s lhs))])]
+             (cond [(and (num? c1) (num? c2))
+                    (format "  movl ~a, ~a~n"
+                            (if ((case op [(<) <] [(<=) <=] [(=) =]) c1 c2)
+                                (asm-s 1)
+                                (asm-s 0))
+                            (asm-s lhs))]
+                   [(num? c1)
+                    (format "  cmpl ~a, ~a~n  ~a ~a~n  andl ~a, ~a~n"
+                            (asm-s c1) (asm-s c2)
+                            (case op [(<) "setg"] [(<=) "setge"] [(=) "sete"]) (asm-s-lsb lhs)
+                            (asm-s 1) (asm-s lhs))]
+                   [else
+                    (format "  cmpl ~a, ~a~n  ~a ~a~n  andl ~a, ~a~n"
+                            (asm-s c2) (asm-s c1)
+                            (case op [(<) "setl"] [(<=) "setle"] [(=) "sete"]) (asm-s-lsb lhs)
+                            (asm-s 1) (asm-s lhs))])]
     [l1s-label (lbl)
-                (format "  ~a:~n" (asm-s lbl))]
+               (format "  ~a:~n" (asm-s lbl))]
     [l1s-goto (lbl)
-               (format "  jmp ~a~n" (asm-s lbl))]
+              (format "  jmp ~a~n" (asm-s lbl))]
     [l1s-cjump (c1 op c2 lbl1 lbl2)
-                (cond [(and (num? c1) (num? c2))
-                       (format "  jmp ~a~n"
-                               (if ((case op [(<) <] [(<=) <=] [(=) =]) c1 c2)
-                                   (asm-s lbl1)
-                                   (asm-s lbl2)))]
-                      [(num? c1)
-                       (format "  cmpl ~a, ~a~n  ~a ~a~n  jmp ~a~n"
-                               (asm-s c1) (asm-s c2)
-                               (case op [(<) "jg"] [(<=) "jge"] [(=) "je"]) (asm-s-lsb lbl1)
-                               (asm-s lbl2))]
-                      [else
-                       (format "  cmpl ~a, ~a~n  ~a ~a~n  jmp ~a~n"
-                               (asm-s c2) (asm-s c1)
-                               (case op [(<) "jl"] [(<=) "jle"] [(=) "je"]) (asm-s-lsb lbl1)
-                               (asm-s lbl2))])]
+               (cond [(and (num? c1) (num? c2))
+                      (format "  jmp ~a~n"
+                              (if ((case op [(<) <] [(<=) <=] [(=) =]) c1 c2)
+                                  (asm-s lbl1)
+                                  (asm-s lbl2)))]
+                     [(num? c1)
+                      (format "  cmpl ~a, ~a~n  ~a ~a~n  jmp ~a~n"
+                              (asm-s c1) (asm-s c2)
+                              (case op [(<) "jg"] [(<=) "jge"] [(=) "je"]) (asm-s-lsb lbl1)
+                              (asm-s lbl2))]
+                     [else
+                      (format "  cmpl ~a, ~a~n  ~a ~a~n  jmp ~a~n"
+                              (asm-s c2) (asm-s c1)
+                              (case op [(<) "jl"] [(<=) "jle"] [(=) "je"]) (asm-s-lsb lbl1)
+                              (asm-s lbl2))])]
     [l1s-call (dst)
-               (let ([new-label (lblfn)])
-                 (format "  pushl $~a~n  pushl ~a~n  movl ~a, ~a~n  jmp ~a~n  ~a:~n"
-                         (asm-s new-label)
-                         (asm-s 'ebp)
-                         (asm-s 'esp) (asm-s 'ebp)
-                         (if (label? dst)
-                             (asm-s dst)
-                             (string-append "*" (asm-s dst)))
-                         (asm-s new-label)))]
-    [l1s-tcall (dst)
-                (format "  movl ~a, ~a~n  jmp ~a~n"
-                        (asm-s 'ebp) (asm-s 'esp)
+              (let ([new-label (lblfn)])
+                (format "  pushl $~a~n  pushl ~a~n  movl ~a, ~a~n  jmp ~a~n  ~a:~n"
+                        (asm-s new-label)
+                        (asm-s 'ebp)
+                        (asm-s 'esp) (asm-s 'ebp)
                         (if (label? dst)
                             (asm-s dst)
-                            (string-append "*" (asm-s dst))))]
+                            (string-append "*" (asm-s dst)))
+                        (asm-s new-label)))]
+    [l1s-tcall (dst)
+               (format "  movl ~a, ~a~n  jmp ~a~n"
+                       (asm-s 'ebp) (asm-s 'esp)
+                       (if (label? dst)
+                           (asm-s dst)
+                           (string-append "*" (asm-s dst))))]
     [l1s-return ()
-                 (format "  movl ~a, ~a~n  popl ~a~n  ret~n"
-                         (asm-s 'ebp) (asm-s 'esp)
-                         (asm-s 'ebp))]
+                (format "  movl ~a, ~a~n  popl ~a~n  ret~n"
+                        (asm-s 'ebp) (asm-s 'esp)
+                        (asm-s 'ebp))]
     [l1s-print (lhs arg1)
-                (format "  pushl ~a~n  call l1_print~n  addl ~a, ~a~n"
-                        (asm-s arg1)
-                        (asm-s 4) (asm-s 'esp))]
+               (format "  pushl ~a~n  call l1_print~n  addl ~a, ~a~n"
+                       (asm-s arg1)
+                       (asm-s 4) (asm-s 'esp))]
     [l1s-alloc (lhs arg1 arg2)
-                (format "  pushl ~a~n  pushl ~a~n  call l1_alloc~n  addl ~a, ~a~n"
-                        (asm-s arg2)
-                        (asm-s arg1)
-                        (asm-s 8) (asm-s 'esp))]
+               (format "  pushl ~a~n  pushl ~a~n  call l1_alloc~n  addl ~a, ~a~n"
+                       (asm-s arg2)
+                       (asm-s arg1)
+                       (asm-s 8) (asm-s 'esp))]
     [l1s-arrayerr (lhs arg1 arg2)
-                   (format "  pushl ~a~n  pushl ~a~n  call l1_arrayerr~n  addl ~a, ~a~n"
-                           (asm-s arg2)
-                           (asm-s arg1)
-                           (asm-s 8) (asm-s 'esp))]))
+                  (format "  pushl ~a~n  pushl ~a~n  call l1_arrayerr~n  addl ~a, ~a~n"
+                          (asm-s arg2)
+                          (asm-s arg1)
+                          (asm-s 8) (asm-s 'esp))]))
 
 (define-with-contract (asm-s s)
   (L2-s? . -> . string?)
