0989186ce4ab49fcbc412bccd22d9b6d9a921231
Geoff Hill <geoff@geoffhill.org>
2011-06-05 23:12:15 -0500

    driver now copies the asm file for refernece

--- driver.sh
+++ driver.sh
@@ -9,11 +9,12 @@ if [ `uname` == "Darwin" ]
   else
     RACKET_PATH="/opt/racket-5.1/bin/racket"
     AS="as"
-    CC="gcc4 -static -O0"
+    CC="gcc4 -static -O2"
 fi
 
 FPREFIX=`echo $RANDOM`
 EXEC="a.out"
+ASMCPY="a.asm"
 
 if [ -z "$1" ];
   then
@@ -21,8 +22,9 @@ if [ -z "$1" ];
   else
     if [ "$ASSEMBLE" == "1" ]
       then
+        rm -f "$EXEC" "$ASMCPY"
         "$RACKET_PATH" -tm "$DIR/driver.rkt" "$COMMAND" "$1" > "$FPREFIX.asm"
-        # TODO: fix GCC optimization
+        cp "$FPREFIX.asm" "$ASMCPY"
         $AS -o "$FPREFIX.o" "$FPREFIX.asm"
         $CC -o "$EXEC" "L1-runtime.c" "$FPREFIX.o"
         chmod +x "$EXEC"
